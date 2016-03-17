34d75a8380434f200a5c1d3b675fb595871353e4
risan <risanbagja@gmail.com>
2016-03-17 20:14:38 +0100

    Add dotenv package.

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
     "log"
     "net/http"
+    "github.com/joho/godotenv"
 )
 
 func main() {
+    err := godotenv.Load()
+
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
     router := NewRouter()
 
     log.Fatal(http.ListenAndServe(":8000", router))
